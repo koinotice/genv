1c8f481a799bf07fd9866fb53ac261ec12df7b07
Ben Getsug <ben.getsug@wheniwork.com>
2017-08-10 13:00:02 -0500

    output tweaks

--- core/func/output.sh
+++ core/func/output.sh
@@ -56,7 +56,7 @@ speak_greeting() {
 		export HARPOON_SPEECH_RATE=225
 		name=$(finger `whoami` | awk -F: '{ print $3 }' | head -n1 | sed 's/^ //' | cut -d " " -f 1)
 		msg="Hey ${name}! ${GREETING}"
-		speak "${msg}"
+		$(sleep 5 && speak "${msg}") &
 		if [[ ${HARPOON_SPEECH} == false ]]; then
 			echo -e "\n${COLOR_LIGHT_PURPLE}${msg}${COLOR_NC}\n"
 		fi
