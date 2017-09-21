bc2c39cba047ed6a8ad904cc460a67775d4fa36e
Brian Fenton <brian@brianfenton.us>
2017-09-21 14:05:10 -0500

    adding redis-commander service (#13)
    * adding redis-commander service
    
    * removing unneeded build param

---
+++ services/redis-commander/bootstrap.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+if [ ! ${TRAEFIK_ACME:-} ]; then
+	export REDIS_COMMANDER_HOSTS=redis-commander.harpoon.dev
+fi
+
+if [ ${CUSTOM_DOMAINS:-} ]; then
+	for i in "${CUSTOM_DOMAINS[@]}"; do
+		export REDIS_COMMANDER_HOSTS+=",redis-commander.${i}"
+	done
+fi
+
+#% 🔺 REDIS_COMMANDER_VERSION %% Redis Commander Docker image version %% latest
+if [ ! -v REDIS_COMMANDER_VERSION ]; then
+	export REDIS_COMMANDER_VERSION=latest
+fi
+
+if [ ! ${REDIS_HOSTS:-} ]; then
+	export REDIS_HOSTS=redis
+fi
+
+if [ ! ${REDIS_PORT:-} ]; then
+	export REDIS_PORT=6379
+fi
